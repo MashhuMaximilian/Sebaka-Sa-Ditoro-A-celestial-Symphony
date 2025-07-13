687dd656030a3abb1447eae397a4df6f95a80bf4
Marius Ion <m.catalin.ion@gmail.com>
2025-07-13 21:36:40 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/celestial-symphony.tsx
+++ src/components/celestial-symphony.tsx
@@ -144,15 +144,15 @@ const CelestialSymphony = ({
         `;
         const fragmentShader = `
           varying vec3 vUv;
-          void main() {
-            float angle = atan(vUv.y, vUv.x);
-            float hue = (angle + 3.14159) / (2.0 * 3.14159);
-            gl_FragColor = vec4(hsv2rgb(vec3(hue, 0.7, 1.0)), 0.7);
-          }
           vec3 hsv2rgb(vec3 c) {
             vec4 K = vec4(1.0, 2.0 / 3.0, 1.0 / 3.0, 3.0);
             vec3 p = abs(fract(c.xxx + K.xyz) * 6.0 - K.www);
             return c.z * mix(K.xxx, clamp(p - K.xxx, 0.0, 1.0), c.y);
+          }
+          void main() {
+            float angle = atan(vUv.y, vUv.x);
+            float hue = (angle + 3.14159) / (2.0 * 3.14159);
+            gl_FragColor = vec4(hsv2rgb(vec3(hue, 0.7, 1.0)), 0.7);
           }
         `;
 
